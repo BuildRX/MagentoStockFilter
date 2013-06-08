0586c9baa02a3c6670be66f1bfd34e0ed757032c
Matt Dunbar <matt@buildrx.com>
2013-06-08 05:52:58 -0400

    Removed Legacy Code, Added Documentation

--- app/code/community/BRX/Stockfilter/Model/Observer.php
+++ app/code/community/BRX/Stockfilter/Model/Observer.php
@@ -1,26 +1,36 @@
 <?php
+/**
+ * BuildRX
+ *
+ * NOTICE OF LICENSE
+ * 
+ * This code is part of BuildRX's internal libraries.
+ * Unless licensed otherwise you may only use this extension in Magento stores worked on by BuildRX.
+ *
+ * @category    BRX
+ * @package     BRX_Stockfilter
+ * @copyright   Copyright (c) 2013 BuildRX (http://www.buildrx.com)
+ */
+
+/**
+ * Stockfilter observer
+ *
+ * @category    BRX
+ * @package     BRX_Stockfilter
+ * @author      Matt Dunbar <matt@buildrx.com>
+ */
 class BRX_Stockfilter_Model_Observer {
+    /**
+     * Before loading a collection filter out of stock items if necessary (based on the instockonly URL param).
+     *
+     * @param Varien_Event_Observer $observer
+     * @return void
+     */
     public function catalogProductCollectionLoadBefore(Varien_Event_Observer $observer) {
         if (Mage::app()->getFrontController()->getRequest()->getParam('instockonly', 0)) {
             $collection = $observer->getCollection();
             Mage::getSingleton('cataloginventory/stock')->addInStockFilterToCollection($collection);
-            //Mage::log('Collection Filtered By Stock;: '.sizeof($collection));
-            /*
-            $oCollection = Mage::getModel('cataloginventory/stock_item')
-                ->getCollection()
-                ->addFieldToFilter('is_in_stock', 0);
-            $oProducts = array();
-            foreach ($oCollection as $_collection) {
-                $oProducts[] = $_collection->getProductId();
-            }
-            if (!empty($oProducts))
-                $collection->addIdFilter($oProducts, true);
-
-            */
-
         }
     }
 }
-
-
 ?>
